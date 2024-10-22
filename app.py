96ef1ba75fbae3b114d1c5894d5d169c6b50178a
Abednego Steven Sihite <abednego.steven@ibm.com>
2024-10-22 13:25:00 +0700

    changes about search filter

--- app.py
+++ app.py
@@ -13,9 +13,14 @@ def index():
 @app.route('/search', methods=['POST'])
 def search():
     search_term = request.form['search_term']
-    search_results = data[(data['Market'].str.contains(search_term, case=False)) |
-                          (data['ASEAN Market'].str.contains(search_term, case=False))]
+    
+    # Search for the client in the 'Market' or other relevant columns
+    search_results = data[(data['Market'].str.contains(search_term, case=False, na=False)) |
+                          (data['ASEAN Market'].str.contains(search_term, case=False, na=False))]  # Add more columns if needed
+
+    # Render results if found
     return render_template('index.html', tables=[search_results.to_html(classes='data')], search_term=search_term)
+
 
 # Necessary for vercel deployment
 if __name__ == "__main__":
